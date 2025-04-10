5b6f796988548b99e75943919822e8311e8dce6f
Tom Camp <tomcamp@protonmail.ch>
2025-04-09 20:26:46 -0400

    Updating project structure.

--- app/settings.py
+++ app/settings.py
@@ -1,8 +1,9 @@
 import os
+from distutils.util import strtobool
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-key-please-change")
-FLASK_DEBUG = os.environ.get("DEBUG", True)
+FLASK_DEBUG = bool(strtobool(os.environ.get("DEBUG", "true")))
